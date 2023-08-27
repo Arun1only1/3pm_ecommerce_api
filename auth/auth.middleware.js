a3a8265aa294e822fe1162d0610a91a0e84ab476
Arun1only1 <arun1nly1@gmail.com>
2023-08-27 16:24:57 +0545

    feat: delete and details of product

--- auth/auth.middleware.js
+++ auth/auth.middleware.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
 import { User } from "../user/user.model.js";
 
+// seller role check
 export const isSeller = async (req, res, next) => {
   try {
     // extract token from headers
@@ -30,7 +31,7 @@ export const isSeller = async (req, res, next) => {
     }
 
     // add user to req.userInfo
-    req.userInfo = user;
+    req.loggedInUser = user;
 
     // call next function
     next();
@@ -39,6 +40,7 @@ export const isSeller = async (req, res, next) => {
   }
 };
 
+// buyer role check
 export const isBuyer = async (req, res, next) => {
   try {
     // extract token from headers
@@ -68,7 +70,41 @@ export const isBuyer = async (req, res, next) => {
     }
 
     // add user to req.userInfo
-    req.userInfo = user;
+    req.loggedInUser = user;
+
+    // call next function
+    next();
+  } catch (error) {
+    return res.status(401).send({ message: "Unauthorized." });
+  }
+};
+
+// just logged in user
+export const isUser = async (req, res, next) => {
+  try {
+    // extract token from headers
+    const authorization = req?.headers?.authorization;
+    const splittedArray = authorization?.split(" ");
+    const token = splittedArray?.length === 2 && splittedArray[1];
+
+    // if not token, terminate
+    if (!token) {
+      throw new Error("Unauthorized.");
+    }
+
+    // decrypt token using jwt.verify(token, secret_key)
+    const userData = jwt.verify(token, process.env.JWT_ACCESS_TOKEN_SECRET_KEY);
+
+    // find user from email decrypted from token
+    const user = await User.findOne({ email: userData.email });
+
+    // if not user, terminate
+    if (!user) {
+      throw new Error("Unauthorized.");
+    }
+
+    // add user to req.userInfo
+    req.loggedInUser = user;
 
     // call next function
     next();
--- product/product.route.js
+++ product/product.route.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { isSeller } from "../auth/auth.middleware.js";
+import { isSeller, isUser } from "../auth/auth.middleware.js";
 import { Product } from "./product.model.js";
 import { addProductValidationSchema } from "./product.validation.js";
+import mongoose from "mongoose";
 
 const router = express.Router();
 
@@ -19,7 +20,7 @@ router.post("/product/add", isSeller, async (req, res) => {
   }
 
   //   add seller id
-  newProduct.sellerId = req.userInfo._id;
+  newProduct.sellerId = req.loggedInUser._id;
 
   // create new product
   await Product.create(newProduct);
@@ -28,4 +29,68 @@ router.post("/product/add", isSeller, async (req, res) => {
   return res.status(201).send({ message: "Product is added successfully." });
 });
 
+// delete product
+router.delete("/product/delete/:id", isSeller, async (req, res) => {
+  // extract id from params
+  const productId = req.params.id;
+
+  // validate id for mongo id validity
+  const isValidMongoId = mongoose.Types.ObjectId.isValid(productId);
+
+  // if not valid mongo id, terminate
+  if (!isValidMongoId) {
+    return res.status(400).send({ message: "Invalid mongo id." });
+  }
+
+  // find product
+  const product = await Product.findOne({ _id: productId });
+
+  // if, not product, terminate
+
+  if (!product) {
+    return res.status(404).send({ message: "Product does not exist." });
+  }
+
+  // check for product ownership
+  // loggedInUser id must match with product's sellerId
+  const isOwnerOfProduct = product.sellerId.equals(req.loggedInUser._id);
+
+  // if no match, not allowed to delete
+  if (!isOwnerOfProduct) {
+    return res
+      .status(403)
+      .send({ message: "You are not owner of this product." });
+  }
+
+  // delete product
+  await Product.deleteOne({ _id: productId });
+
+  // send response
+  return res.status(200).send({ message: "Product deleted successfully." });
+});
+
+// get product details
+router.get("/product/details/:id", isUser, async (req, res) => {
+  // extract id from params
+  const productId = req.params.id;
+
+  // check for mongo id validity
+  const isValidMongoId = mongoose.Types.ObjectId.isValid(productId);
+
+  // if not valid, terminate
+  if (!isValidMongoId) {
+    return res.status(400).send({ message: "Invalid mongo id." });
+  }
+
+  // find product
+  const product = await Product.findOne({ _id: productId });
+
+  // if not product, terminate
+  if (!product) {
+    return res.status(404).send({ message: "Product does not exist." });
+  }
+
+  // return product
+  return res.status(200).send(product);
+});
 export default router;
