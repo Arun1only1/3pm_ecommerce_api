33479f4e7d4b38bad182f8539c1d7c275206309a
Arun1only1 <arun1nly1@gmail.com>
2023-09-12 16:42:16 +0545

    chore:env included

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import express from "express";
 import { dbConnect } from "./db.connection.js";
 import userRoutes from "./user/user.route.js";
 import productRoutes from "./product/product.route.js";
-
+import cors from "cors";
 const app = express();
+
+app.use(cors({ origin: "*" }));
 // to make app understand json
 app.use(express.json());
 
@@ -13,6 +15,9 @@ dbConnect();
 // register routes
 app.use(userRoutes);
 app.use(productRoutes);
+
+// allow cors
+// CORS => Cross Origin Resource Sharing
 
 const port = process.env.PORT;
 
